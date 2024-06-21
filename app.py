77079eb6b527f0e4920e6db9d3c5acfcf3de33a6
Ahmed Ashraf <ahmed_ashraf15@icloud.com>
2024-06-21 21:20:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # Import the required libraries
 import streamlit as st
 from PIL import Image
+import matplotlib.pyplot as plt
 from ultralytics import YOLO
-from utils.py import detect_objects, load_image
+from utils import detect_objects, load_image, draw_bounding_boxes
 
 # Load the pre-trained YOLOv8 models
 models = {
@@ -23,7 +24,6 @@ if obj_detect:
     st.switch_page("Images with Detected Objects")
 
 
-
 # File uploader widget
 uploaded_file = st.file_uploader("Choose an image to analyze...", type=["jpg", "jpeg", "png"])
 
@@ -31,8 +31,6 @@ if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
-
-
 
     # Button to trigger image analysis
     if st.button("Analyse Image"):
@@ -53,8 +51,24 @@ if uploaded_file is not None:
             print(f"Number of detected objects: {len(results['detections'])}\n")
             print("Detected Objects:")
             labels = []
+
             for label, confidence in results["detections"]:
                 print(f'Label: {label}, Confidence: {confidence * 100:.2f}%')
                 labels.append(label)
             print(f"Names of the components detected in the uploaded image: {labels}")
             print('\n')
+
+            image_with_bb = image.copy()
+
+            # Draw bounding boxes on the image for specific model
+            image_with_bounding_boxes = draw_bounding_boxes(image_with_bb, all_results[model_name]["detections"],
+                                                            all_results[model_name]["Boxes"])
+
+            # Display the image with bounding boxes
+            plt.title(f"Bounding Boxes for {model_name}")
+            plt.imshow(image_with_bounding_boxes)
+            plt.axis('off')
+            plt.show()
+
+            # clear the image after each plot
+            image = image.copy()
